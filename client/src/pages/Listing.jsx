e30d6040bd02b863e943853ee92e8792a8885544
arai08 <rehaan.arai17@gmail.com>
2025-04-12 23:23:03 -0400

    Add Contact landlord functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
@@ -13,15 +14,18 @@ import {
     FaParking,
     FaShare,
   } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 export default function Listing() {
 
     SwiperCore.use([Navigation]);
     const params = useParams();
+    const { currentUser } = useSelector((state) => state.user);
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
 
     useEffect(() => {
         
@@ -122,6 +126,14 @@ export default function Listing() {
                             {listing.furnished ? 'Furnished' : 'Unfurnished'}
                         </li>
                     </ul>
+
+                    {currentUser && listing.userRef !== currentUser._id && !contact && (
+                        <button onClick={() => setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'> Get in touch </button>
+                    )}
+
+                    {contact && (
+                        <Contact listing={listing} />
+                    )}
                 </div>
 
             </div>
